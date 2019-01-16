5c3de6a85fef2f8cae4fec437e79df5c27ed7728
janos erdos <erdosj@gmail.com>
2019-01-16 13:03:43 +0100

    release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.erdos/erdos.oauth "0.1.2"
+(defproject io.github.erdos/erdos.oauth "0.1.3"
   :description "OAuth library for Ring in Clojure"
   :url "https://github.com/erdos/erdos.oauth"
   :license {:name "Eclipse Public License"

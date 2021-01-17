ecb0d321250281e82a5555709278ea032ad6007c
Janos Erdos <janos@transferwise.com>
2021-01-17 11:07:19 +0100

    feat: httpkit update, release version 0.1.6

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject io.github.erdos/erdos.oauth "0.1.5"
+(defproject io.github.erdos/erdos.oauth "0.1.6"
   :description "OAuth library for Ring in Clojure"
   :url "https://github.com/erdos/erdos.oauth"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.3.0"]
+                 [http-kit "2.5.1"]
                  [cheshire "5.10.0"]]
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :username :env/clojars_user
